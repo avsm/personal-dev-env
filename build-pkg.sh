b1756140c955c40ea78cdba9a9c41e6821a29276
Anil Madhavapeddy <anil@recoil.org>
2016-01-03 11:18:14 +0000

    build signed packages from scratch

--- build-pkg.sh
+++ build-pkg.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+rm -rf ~/src/git/avsm/aports-packages
+mkdir -p ~/src/git/avsm/aports-packages
+
 docker run \
   -v ~/src/git/avsm/aports-packages:/home/avsm/packages \
   -v ~/.alpine:/home/avsm/.abuild \
   -ti avsm/personal-dev-env:alpine sh -c \
-    "git clone -b ocaml-ports git://github.com/avsm/aports && cd aports/testing/opam && abuild -R"
+    "sudo cp ~/.abuild/*.pub /etc/apk/keys && git clone -b ocaml-ports git://github.com/avsm/aports && cd aports/testing/opam && abuild -R"
